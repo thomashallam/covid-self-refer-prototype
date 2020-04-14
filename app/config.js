437b06670b840146862ea65ea16d1671f468f768
thomashallam <thomas.j.hallam@googlemail.com>
2020-04-14 09:57:45 +0100

    Commit name changes

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Covid-19 Virus Testing',
+  serviceName: 'Test for Coronavirus',
 
   // Default port that prototype runs on
   port: '3000',
